4f1a3fc4ea0c2e7491214211cc6186c8ca4b17d3
Robin Hübner <profan@prfn.se>
2017-07-29 23:28:46 +0200

    lol, game change much

--- Player.gd
+++ Player.gd
@@ -1,67 +1,44 @@
-extends Node2D
-
-var mov_direction = Vector2(1, 0)
-var mov_speed = 128 # pixels per second
-
-var cur_frame = 0
-var cur_time_per_frame = 0.1
-var cur_time = 0
-var cur_moving = true
+extends KinematicBody2D
 
 onready var sprite = get_node("Sprite")
+
+var mov_speed = 64 # pixels per second
+var ship_vel = Vector2(0, 0)
+var ship_max_vel = 100 # pixels per second
+var ship_accel = 25 # pixels per second.. per second?
+var ship_turn_speed = deg2rad(360) # degrees per second.. i guess?
+var ship_mass = 61
+
+var ship_dir = Vector2(1, 0)
 
 func _ready():
 	set_fixed_process(true)
 	
+func turn_towards(delta, pos):
+	
+	var target_dir = (pos - get_pos()).normalized()
+	var cpd = target_dir.dot(ship_dir.rotated(deg2rad(90)))
+
+	var as = asin(cpd)
+	var ca = clamp(as, (-ship_turn_speed) * delta, (ship_turn_speed) * delta)
+
+	ship_dir = ship_dir.rotated(-ca)
+	
 func _fixed_process(delta):
-
+	
 	var mov_delta = Vector2(0, 0)
-	var mov_direction = Vector2(1, 0)
+	turn_towards(delta, get_viewport().get_mouse_pos())
+
+	if Input.is_action_pressed("player_move_forwards"):
+		mov_delta += -ship_dir
+	elif Input.is_action_pressed("player_move_backwards"):
+		mov_delta += ship_dir
+
+	sprite.set_rot(ship_dir.angle())
+
+	var new_vel = (ship_vel + (mov_delta * (delta * ship_accel))).clamped(ship_max_vel)
+	var diff = new_vel - ship_vel
+	ship_vel += diff
 	
-	# move towards mouse
-	mov_direction = (get_viewport().get_mouse_pos() - get_global_pos()).normalized()
-	
-	if Input.is_action_pressed("player_move_forwards") or Input.is_action_pressed("player_move_backwards") or Input.is_action_pressed("player_move_left") or Input.is_action_pressed("player_move_right"):
-		if Input.is_action_pressed("player_move_forwards"):
-			mov_delta.y -= 1 * (mov_speed * delta)
-			# mov_delta = mov_direction * (mov_speed * delta)
-			cur_moving = true
-		elif Input.is_action_pressed("player_move_backwards"):
-			# mov_delta = (-mov_direction) * (mov_speed * delta)
-			mov_delta.y += 1 * (mov_speed * delta)
-			cur_moving = true
-		if Input.is_action_pressed("player_move_left"):
-			mov_delta.x -= 1 * (mov_speed * delta)
-			cur_moving = true
-		elif Input.is_action_pressed("player_move_right"):
-			mov_delta.x += 1 * (mov_speed * delta)
-			cur_moving = true
-	else:
-		cur_moving = false
-	
-
-	if Input.is_action_pressed("player_attack"):
-		pass
-
-	cur_time = cur_time + delta
-
-	if cur_moving and cur_frame != 6:
-		if cur_time > cur_time_per_frame:
-			cur_frame += 1
-			cur_time = 0
-	else:
-		if cur_moving:
-			cur_frame = 1
-		else:
-			cur_frame = 0
-
-	# set frame shit
-	sprite.set_frame(cur_frame)
-	sprite.set_rot(sprite.get_global_pos().angle_to_point(get_viewport().get_mouse_pos()))
-
-	# do teh moves
-	move_local_x(mov_delta.x)
-	move_local_y(mov_delta.y)
-	
-func _process(delta):
-	pass
+	self.move(ship_vel)
+	ship_vel = ship_vel / (delta * ship_mass)
