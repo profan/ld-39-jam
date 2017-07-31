4dbf17fb46c71a07909b655b2eebcd1ee2978443
Robin Hübner <profan@prfn.se>
2017-07-31 18:23:46 +0200

    cruddy enemy grunt ship sprite?

--- EnemyGrunt.gd
+++ EnemyGrunt.gd
@@ -1,10 +1,16 @@
 extends KinematicBody2D
 
+var grunt_rot_speed = deg2rad(22.5) # degrees per second
+
 func _ready():
 	var t = get_tree()
 	var r = t.get_root()
 	var n = r.get_node("Game/MinimapControl")
 	n.register_entity(self)
-	
+	set_process(true)
+
 func type():
 	return "EnemyGrunt"
+	
+func _process(delta):
+	rotate(grunt_rot_speed * delta)
